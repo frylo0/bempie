c8799aa8d82cc8880053416ef3b671408184c96d
frity <nikonovfedor36936@gmail.com>
2020-11-29 20:51:19 +0300

    crebemp: tab-size 3, default js content

--- crebemp.js
+++ crebemp.js
@@ -1,28 +1,28 @@
 async function crebemp(argv) {
-  const fs = require('fs'),
-    { exec } = require('child_process'),
-    { resolve } = require('path'),
-    readline = require('readline').createInterface(process.stdin, process.stdout);
+   const fs = require('fs'),
+      { exec } = require('child_process'),
+      { resolve } = require('path'),
+      readline = require('readline').createInterface(process.stdin, process.stdout);
 
-  async function ask(question) {
-    return new Promise(resolve => {
-      readline.question(question, answer => {
-        resolve(answer);
+   async function ask(question) {
+      return new Promise(resolve => {
+         readline.question(question, answer => {
+            resolve(answer);
+         });
       });
-    });
-  }
+   }
 
-  const dir = process.cwd();
+   const dir = process.cwd();
 
-  fs.createFilesSync = function (files) {
-    for (const filename in files) {
-      const filecontent = files[filename];
-      fs.writeFileSync(resolve(dir, filename), filecontent);
-    }
-  };
+   fs.createFilesSync = function (files) {
+      for (const filename in files) {
+         const filecontent = files[filename];
+         fs.writeFileSync(resolve(dir, filename), filecontent);
+      }
+   };
 
-  function help() {
-    let help = `
+   function help() {
+      let help = `
     > crebemp block_name [element_name]    
 
     where:
@@ -37,57 +37,61 @@ async function crebemp(argv) {
         create ./block_dir/element_dir
         create element_files (pug, sass, js) in ./block_dir/element_dir
     `;
-    console.log(help);
-  }
+      console.log(help);
+   }
 
-  async function main() {
-    if (argv.length == 0 || argv.some(arg => arg.match(/(--help|\/\?)/))) {
-      help(); return;
-    }
+   async function main() {
+      if (argv.length == 0 || argv.some(arg => arg.match(/(--help|\/\?)/))) {
+         help(); return;
+      }
 
-    let target = argv[0];
-    const block = target;
-    const element = argv[1];
+      let target = argv[0];
+      const block = target;
+      const element = argv[1];
 
-    let blockName = block.split(/[\/\\]/).pop();
+      let blockName = block.split(/[\/\\]/).pop();
 
-    if (element) {
-      target = resolve(target, '__' + element);
-    }
+      if (element) {
+         target = resolve(target, '__' + element);
+      }
 
-    if (fs.existsSync(target)) {
-      if ((await ask(`BEM ${element ? 'Element' : 'Block'} already exist. Replace? (yes|y|да|д): `)).match(/(yes|y|да|д)/i)) {
-        fs.rmdirSync(target, { recursive: true });
-      } else return null;
-    }
+      if (fs.existsSync(target)) {
+         if ((await ask(`BEM ${element ? 'Element' : 'Block'} already exist. Replace? (yes|y|да|д): `)).match(/(yes|y|да|д)/i)) {
+            fs.rmdirSync(target, { recursive: true });
+         } else return null;
+      }
 
-    if (!fs.existsSync(block) && element) {
-      console.log(`Warning: Block "${block}" was not found => created new folder.`);
-      fs.mkdirSync(block);
-    }
-    fs.mkdirSync(target);
+      if (!fs.existsSync(block) && element) {
+         console.log(`Warning: Block "${block}" was not found => created new folder.`);
+         fs.mkdirSync(block);
+      }
+      fs.mkdirSync(target);
 
-    if (element) {
-      blockName = `${blockName}__${element}`;
-    }
-    const blockFilenameBasis = `${target}/${blockName}`;
-    fs.createFilesSync({
-      [blockFilenameBasis + '.pug']: `mixin ${blockName}()
-  .${blockName}&attributes(attributes)
-    `,
-      [blockFilenameBasis + '.sass']: `.${blockName}
-  `,
-      [blockFilenameBasis + '.js']: ''
-    });
+      if (element) {
+         blockName = `${blockName}__${element}`;
+      }
+      const blockFilenameBasis = `${target}/${blockName}`;
+      fs.createFilesSync({
+         [blockFilenameBasis + '.pug']: `mixin ${blockName}()
+   .${blockName}&attributes(attributes)
+      `,
+         [blockFilenameBasis + '.sass']: `.${blockName}
+   `,
+         [blockFilenameBasis + '.js']: `$(document).ready(() => {
+   const pref = '${blockName}'; // prefix for current folder
+   
+   
+});`
+      });
 
-    return;
-  }
+      return;
+   }
 
-  await main();
+   await main();
 }
 
 if (require.main == module) {
-  crebemp(process.argv.slice(2)).then(() => process.exit());
+   crebemp(process.argv.slice(2)).then(() => process.exit());
 } else {
-  module.exports = crebemp;
+   module.exports = crebemp;
 }
